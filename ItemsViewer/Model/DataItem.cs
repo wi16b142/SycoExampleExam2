631a3378017af006dcbf73a49b0435134d1447ba
Sascha Böck <wi16b142@technikum-wien.at>
2018-05-28 17:08:07 +0200

    Add project files.

---
+++ ItemsViewer/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace ItemsViewer.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
